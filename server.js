924ce770f801e10cab93124a2f5b4e641e8ee899
Mauricio Jimenez <mauriciobenjamin@gmail.com>
2021-05-29 17:21:22 -0500

    Se agrego la primera ruta GET

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const app = express();
 
+const mockUserDate = [
+    {name: 'Marco'},
+    {name: 'Gil'}
+];
+
+app.get('/usuarios', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Exito obteniendo a los usuarios',
+        users: mockUserDate
+    });
+});
+
 app.listen(8000, () => console.log('el servidor esta funcionando'));
